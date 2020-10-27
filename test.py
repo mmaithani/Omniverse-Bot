9e5f667bfb19ed8edb260999073805509225385c
Mohit <mohitmaithani81@gmail.com>
2020-10-27 10:02:00 +0530

    update tweepy test code

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key=''
-consumer_secret=''
-access_token=''
-access_token_secret=''
+consumer_key='5rIQ2N7lBKijy7mucv8KO'
+consumer_secret='uHpPL0GMO30krGBCS3uK1ZKToBsBvn58RoBTx9'
+access_token='13192047395238543377cOzbdrAPYgd3aPLsxC'
+access_token_secret='Z0lrCVFrZ23puzNw9gcve89'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
